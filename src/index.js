371b9e93ffc7993798ddda316f062d0cf0ff2b1e
MirNO89 <108218701+MirNO89@users.noreply.github.com>
2022-08-30 16:49:25 +0200

    Moved forecast html to js, built function to call forecast and moved conditions into description list

--- src/index.js
+++ src/index.js
@@ -21,6 +21,35 @@ let days = [
 let day = days[now.getDay()];
 todaysTime.innerHTML = `${day}, ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //Search Weather
 function displayWeatherCondition(response) {
   let iconElement = document.querySelector("#icon");
@@ -94,3 +123,4 @@ let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", handleSubmit);
 
 searchCity("Oslo");
+displayForecast();
